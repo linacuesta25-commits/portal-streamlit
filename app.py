f16a09634366f2bdf2c61ed77ee3dd83a0abb982
linacuesta25-commits <lina.cuesta25@gmail.com>
2025-12-20 04:45:46 -0500

    app.py

--- app.py
+++ app.py
@@ -3405,7 +3405,7 @@ def render_spotify_persistente():
             style="border-radius:12px" 
             src="src="https://open.spotify.com/embed/album/3QC8nwNxiwe68Rj96yjgzo?utm_source=generator&theme=0" 
             width="100%" 
-            height="152" 
+            height="380" 
             frameBorder="0" 
             allowfullscreen="" 
             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
@@ -5570,6 +5570,7 @@ else:
     # =====================================================
       
 st.markdown('<div class="bottom-footer">🌙 Que la luz de tu intuición te guíe en este viaje sagrado 🌙</div>', unsafe_allow_html=True)
+
 
 
 
